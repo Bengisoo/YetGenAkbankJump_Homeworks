231f766df36f8d13a2fb9ef9b06bc276dabc4fc8
Bengisoo <zehrabengisu8@gmail.com>
2023-12-06 23:53:04 +0300

    [Fixed] Persistence

--- YetGenAkbankJumpWeek11/Week11_2/Week11_2.Persistence/Contexts/ApplicationDbContext.cs
+++ YetGenAkbankJumpWeek11/Week11_2/Week11_2.Persistence/Contexts/ApplicationDbContext.cs
@@ -6,6 +6,7 @@ using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 using Week11_2.Domain.Entities;
+using Week11_2.Domain.Identity;
 
 namespace Week11_2.Persistence.Contexts
 {
@@ -21,6 +22,9 @@ namespace Week11_2.Persistence.Contexts
 		protected override void OnModelCreating(ModelBuilder modelBuilder)
 		{
 			modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+			modelBuilder.Ignore<User>();
+			modelBuilder.Ignore<Role>();
+			modelBuilder.Ignore<UserSetting>();
 			base.OnModelCreating(modelBuilder);
 		}
 	}
---
+++ YetGenAkbankJumpWeek11/Week11_2/Week11_2.Persistence/Contexts/YetgenIdentityContext.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Reflection.Emit;
+using System.Text;
+using System.Threading.Tasks;
+using Week11_2.Domain.Entities;
+using Week11_2.Domain.Identity;
+
+namespace Week11_2.Persistence.Contexts
+{
+    public class YetgenIdentityContext: IdentityDbContext<User,Role,Guid>
+	{
+        public YetgenIdentityContext(DbContextOptions<YetgenIdentityContext> dbContextOptions) : base(dbContextOptions)
+		{//    YetgenIdentityContext
+
+		}
+		protected override void OnModelCreating(ModelBuilder builder)
+		{
+			builder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+
+			builder.Ignore<Product>();
+			builder.Ignore<Category>();
+			builder.Ignore<ProductCategory>();
+			base.OnModelCreating(builder);
+		}
+
+	}
+}

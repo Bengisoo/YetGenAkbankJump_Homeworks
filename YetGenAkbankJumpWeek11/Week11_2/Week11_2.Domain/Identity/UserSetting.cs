cbd67e8772b92c1d76e82ef843aa7b550d7d1c99
Bengisoo <zehrabengisu8@gmail.com>
2023-12-07 11:02:19 +0300

    [Update] Domain

---
+++ YetGenAkbankJumpWeek11/Week11_2/Week11_2.Domain/Identity/UserSetting.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Week11_2.Domain.Entities;
+
+namespace Week11_2.Domain.Identity
+{
+	public class UserSetting:EntityBase<Guid>
+	{
+		public Guid UserId { get; set; }
+		public User User { get; set; }
+
+		public Int16 TimeZone {  get; set; }
+		public string LanguageCode { get; set; }
+
+	}
+}

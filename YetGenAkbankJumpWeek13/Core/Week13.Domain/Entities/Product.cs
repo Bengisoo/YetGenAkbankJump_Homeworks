c02f30dbfac9d6b7649a9e3525486e54eba33171
Bengisoo <zehrabengisu8@gmail.com>
2023-12-08 22:38:34 +0300

    [Added] Domain Layer
    Added Entities

---
+++ YetGenAkbankJumpWeek13/Core/Week13.Domain/Entities/Product.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Week13.Domain.Common;
+
+namespace Week13.Domain.Entities
+{
+    public class Product: EntityBase
+    {
+        public string Name { get; set; }
+        public decimal Price { get; set; }
+    }
+}
